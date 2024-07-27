fc4a477533990903d5ea763b32e4bb57685bf0bf
eonn3 <89135941+eonn3@users.noreply.github.com>
2024-07-27 20:40:07 +0800

    Update Peanut.sol

--- Peanut.sol
+++ Peanut.sol
@@ -1,18 +1,27 @@
-// SPDX-License-Identifier: MIT 
-// Compatible with OpenZeppelin Contracts ^5.0.0 3 
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol"; 
-import "@openzeppelin/contracts/access/ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Peanut is ERC20, ERC20Burnable, Ownable {
+contract Peanut is ERC20, Ownable {
     constructor(address initialOwner)
         ERC20("Peanut", "PEA")
         Ownable(initialOwner)
     {}
 
-    function mint(address to, uint256 amount) public OnlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
+    }
+
+    function transferPeanuts(address to, uint256 amount) public {
+        address owner = _msgSender();
+        _transfer(owner, to, amount);
+    }
+
+    function burn(uint256 amount) public {
+        address owner = _msgSender();
+        _burn(owner, amount);
     }
 }
